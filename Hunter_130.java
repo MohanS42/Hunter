43affc6631544987acc538ddc0d15c79ab0cbadc
Mohan.s <mohumms007@gmail.com>
2017-10-10 06:11:50 -0700

    Add files via upload

---
+++ Hunter_130.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Hunter_130 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		if(str.equals("Sunday"))
+		{
+			System.out.println("False");
+		}
+		else if(str.equals("Monday")||str.equals("Tuesday")||str.equals("Wednesday")||str.equals("Thursday")||str.equals("Friday")||str.equals("Saturday")){
+			System.out.println("True");
+		}
+		else if(str.equals("monday")||str.equals("tuesday")||str.equals("wednesday")||str.equals("thursday")||str.equals("friday")||str.equals("saturday")){
+			System.out.println("True");
+		}
+		else if(str.equals("mon")||str.equals("tue")||str.equals("wed")||str.equals("thu")||str.equals("fri")||str.equals("sat")){
+			System.out.println("True");
+		}
+		else if(str.equals("Mon")||str.equals("Tue")||str.equals("Wed")||str.equals("Thu")||str.equals("Fri")||str.equals("Sat")){
+			System.out.println("True");
+		}
+	sc.close();}
+}
