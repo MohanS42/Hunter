d4fafd4287d65b4ecd60ad7e536f41350d760173
Mohan.s <mohumms007@gmail.com>
2017-10-10 06:27:26 -0700

    Add files via upload

---
+++ Hunter_133.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Hunter_133 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc= new Scanner(System.in);
+		String str = sc.nextLine();
+		String[] s1=str.split(" ");
+		System.out.print(s1[1]+" "+s1[0]);
+	sc.close();}
+
+}
